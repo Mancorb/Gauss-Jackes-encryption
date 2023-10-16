288ba7e0de00aa300cf0396a4710e8c55d59dd92
Mancorb <andres.m.bosques@gmail.com>
2023-10-15 22:22:35 -0600

    progress on step 2

--- main.py
+++ main.py
@@ -17,6 +17,38 @@ q = result (int) of b/p
 b = p * q + r
 
 """
+class Number :
+    def __init__(self,value,mult=0,equal=None):
+        self.value = value #value of the vurrent number
+        self.multiplier = mult #multiplier of the number
+        self.equivalence = equal #equation it represents
+    
+    def tranasformEquivalence(self, symbol, multiplier=1):
+        """Transform the equivalence equation according to the external symbol and multiplier
+        """
+        #first replace the value in the list for its real equation
+        tempEqui = self.equivalence
+
+        for val in tempEqui:
+            #[a,b]
+            #b = [c,e]
+            #e = [g,f]
+            #g = 2(1)
+            #f = 1(3)
+            #b = [c,[2(1),1(3)]]
+            #[a,[c,e]]
+            if val.
+        pass
+    
+    def extraction(self):
+        #if there is no equivalence equation just return the object
+        if not self.equivalence:
+            return self
+        
+        elif self.equivalence:
+            for val in self.equivalence:
+                val = val.extraction()
+
 
 def getP(kn,I,r_counter):
     for  i in range(len(kn)):
@@ -52,9 +84,21 @@ def getEquation(data):
     return p,q,r,b
 
 def step1(kn,I, b, r_counter):
+    """Obtain all the equations from the matrix
+
+    Args:
+        kn (list): list of the matrix's values
+        I (list): matrix with only ones and zeros where the inverse will be
+        b (int): maximum number of values to use in encryption
+        r_counter (int): row counter to skip to the rwo where this process takes place
+
+    Returns:
+        list: list of equation values
+    """
     #b = p x q + r
     #Make each operation e.g: 89=33(2)+23 -> 33=23(1) + 10
     #obtains p,q,r
+    
     iterations = [getFirstEquation(kn,I,b,r_counter)]
     counter = 0
 
@@ -62,11 +106,16 @@ def step1(kn,I, b, r_counter):
         iterations.append(getEquation(iterations[-1]))
         counter += 1
 
+    #returns p,q,r,b
     return iterations
-    
+
+
+#[p=33, q=2, r=23, b=89]
 """ def step2(iterations):
-    for it in iterations:
-        if it[] """
+    #extract first values in a simple list for better accesibility 
+    values = iterations[0]
+    iterations[0] =  """
+        
 
 
 #create the matrix
